5b9d02d1c77f0d5d542d832bfdd731fd473f77cb
Richard Joffray <richardj3@verifone.com>
2023-04-17 23:10:19 -0700

    close menu on click

--- src/app/components/navbar-rebrand/navbar-rebrand.component.ts
+++ src/app/components/navbar-rebrand/navbar-rebrand.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-navbar-rebrand',
@@ -8,8 +9,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class NavbarRebrandComponent implements OnInit {
   view: string=''
-
-  constructor(private route: ActivatedRoute, private _router: Router) {
+  isClicked: boolean = false;
+  constructor(private route: ActivatedRoute,
+     private _router: Router,
+     @Inject(DOCUMENT) private document: Document) {
     console.log("Route", this.route);
     this.route.queryParams.subscribe(params => {
         this.view = params['view'];
@@ -21,12 +24,16 @@ export class NavbarRebrandComponent implements OnInit {
   }
 
   changePage = (page:string, isSubpage:boolean) => {
+    this.isClicked = true;
     if(isSubpage) {
 
       this._router.navigateByUrl(page + '?view=is-subpage')
     } else {
       this._router.navigateByUrl(page)
     }
+    setTimeout(() =>{
+      this.isClicked = false;
+    },10)
 
   }
 }
